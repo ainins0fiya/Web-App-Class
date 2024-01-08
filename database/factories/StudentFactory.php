73dc0d3b8e9fa5a31081afae4f10240523f56f69
Ainin Sofiya <86847491+ainins0fiya@users.noreply.github.com>
2024-01-09 00:10:23 +0800

    Add files via upload

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Student;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    protected $model = Student::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    public function definition(): array
+    {
+        return [
+            'matric_id' => $this->faker->unique()->numberBetween(1000000, 9999999),
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone_no' => $this->faker->phoneNumber,
+        ];
+    }
+}
